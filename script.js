dbf1d9111334183e72d03f3cac28e46b0b4fce28
Arma <rshevaro@gmail.com>
2026-04-19 21:15:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -139,3 +139,33 @@ document.querySelector('.logo-link').addEventListener('click', function(e){
     behavior: 'smooth'
   });
 });
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+// highlight menu sesuai scroll
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+// smooth scroll
+navLinks.forEach(link => {
+  link.addEventListener("click", function(e){
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
